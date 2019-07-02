b873d53066486adff00fd713d47c0145bf1543ac
bilel-kedidi <bilel.kedidi@gmail.com>
2019-07-02 18:16:22 +0100

    fc

--- init.rb
+++ init.rb
@@ -1,8 +1,8 @@
 Redmine::Plugin.register :redmine_import_user_to_group do
   name 'Redmine Import User To Group plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'bilel kedidi'
+  description 'this plugin import existing user frm csv, xls, xlsx to group ( it use firstname)'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/bilel-kedidi/redmine_import_user_to_group'
+  author_url 'https://github.com/bilel-kedidi/'
 end
